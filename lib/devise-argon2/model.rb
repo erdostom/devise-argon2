af64df32ced616b2eff38d2380bd76a2b8f8757e
Moritz Höppner <moritz.hoeppner2@gmail.com>
2023-10-04 07:00:18 +0200

    Use Devise.pepper as secret key

--- lib/devise-argon2/model.rb
+++ lib/devise-argon2/model.rb
@@ -4,13 +4,15 @@ module Devise
   module Models
     module Argon2
       def valid_password?(password)
-        ::Argon2::Password.verify_password(password, encrypted_password)
+        ::Argon2::Password.verify_password(password, encrypted_password, self.class.pepper)
       end
 
       protected
 
       def password_digest(password)
-        ::Argon2::Password.create(password)
+        hasher_options = { secret: self.class.pepper }
+        hasher = ::Argon2::Password.new(hasher_options)
+        hasher.create(password)
       end
     end
   end
--- spec/devise-argon2_spec.rb
+++ spec/devise-argon2_spec.rb
@@ -7,19 +7,53 @@ describe Devise::Models::Argon2 do
 
   let(:user) { User.new(password: CORRECT_PASSWORD) }
 
+  before do
+    Devise.pepper = nil
+  end
+
   describe 'valid_password?' do
-    it 'validates the correct password' do
-      expect(user.valid_password?(CORRECT_PASSWORD)).to be true
+    shared_examples 'a password is validated if and only if it is correct' do
+      it 'validates the correct password' do
+        expect(user.valid_password?(CORRECT_PASSWORD)).to be true
+      end
+
+      it 'does not validate an incorrect password' do
+        expect(user.valid_password?(INCORRECT_PASSWORD)).to be false
+      end
     end
 
-    it 'does not validate an incorrect password' do
-      expect(user.valid_password?(INCORRECT_PASSWORD)).to be false
+    context 'no pepper' do
+      include_examples 'a password is validated if and only if it is correct'
+    end
+
+    context 'Devise.pepper is set' do
+      before do
+        Devise.pepper = 'pepper'
+      end
+
+      include_examples 'a password is validated if and only if it is correct'
     end
   end
 
   describe 'password_digest' do
-    it 'hashes the given password with Argon2' do
-      expect(Argon2::Password.verify_password(CORRECT_PASSWORD, user.encrypted_password)).to be true
+    context 'no pepper' do
+      it 'hashes the given password with Argon2' do
+        expect(
+          Argon2::Password.verify_password(CORRECT_PASSWORD, user.encrypted_password)
+        ).to be true
+      end
+    end
+
+    context 'Devise.pepper is set' do
+      before do
+        Devise.pepper = 'pepper'
+      end
+
+      it 'uses Devise.pepper as secret key for Argon2' do
+        expect(
+          Argon2::Password.verify_password(CORRECT_PASSWORD, user.encrypted_password, 'pepper')
+        ).to be true
+      end
     end
   end
 end
