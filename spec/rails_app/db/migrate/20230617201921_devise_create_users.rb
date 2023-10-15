9a364363c5bcf8a08c5f23ca3d6b8f0057eeb958
Tamas Erdos <github@tamaserdos.com>
2023-10-15 17:01:20 -0400

    make migration backwards compatible

--- spec/rails_app/db/migrate/20230617201921_devise_create_users.rb
+++ spec/rails_app/db/migrate/20230617201921_devise_create_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class DeviseCreateUsers < ActiveRecord::Migration[7.0]
+class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
--- spec/rails_app/db/schema.rb
+++ spec/rails_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_04_084147) do
+ActiveRecord::Schema.define(version: 2023_10_04_084147) do
   create_table "old_users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
